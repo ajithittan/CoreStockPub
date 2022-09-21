a63e2b2594215705fcb752922dd71425ed0b78b3
ajithittan <ajithittan@gmail.com>
2022-09-20 21:13:11 -0400

    ok
    ok

--- runstockpub.js
+++ runstockpub.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 const redis = require("redis")
 require('dotenv').config()
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const publisher = redis.createClient({url: process.env.REDIS_SERVER_PUBSUB})
 
@@ -9,11 +14,12 @@ publisher.on("connect", () => {console.log("connected")})
 publisher.on("error", (error) => {console.log("error in red",error)})
 
 app.post('/publish', async function (req, res) {
+    //console.log("about to publish",req.body.channel,JSON.stringify({"data":req.body.message}))
     try{
       publisher.publish(req.body.channel, JSON.stringify(req.body.message))
       res.send(true)  
     }catch(error){
-      console.log("Error in publishing to redis - do something",req,error)
+      console.log("Error in publishing to redis - do something",error)
       res.send(false)
     }
 });
